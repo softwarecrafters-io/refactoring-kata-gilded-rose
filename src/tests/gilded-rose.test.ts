771f1782f843b51411dd30e857ea5ef2bac31b8a
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-12-07 20:04:27 +0000

    refactor: lift up conditional

--- src/tests/gilded-rose.test.ts
+++ src/tests/gilded-rose.test.ts
@@ -1,12 +1,12 @@
 import { Item, GildedRose } from '../core/gilded-rose';
 
 describe('The Gilded Rose', () => {
-	xit('updates quality for a new item', () => {
+	it('updates quality for a new item', () => {
 		const gildedRose = new GildedRose([new Item('new item', 0, 0)]);
 
 		const items = gildedRose.updateQuality();
 
-		expect(items[0]).toEqual({"name": "foo", "quality": 0, "sellIn": -1});
+		expect(items[0]).toEqual({"name": "new item", "quality": 0, "sellIn": -1});
 	});
 
 	it('updates quality (using golden master approval technique)', () => {
